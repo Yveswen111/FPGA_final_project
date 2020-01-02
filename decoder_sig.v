b5c710a877f5049811ef5f04e0739b0888b65682
Yveswen111 <45478312+Yveswen111@users.noreply.github.com>
2020-01-02 16:12:00 +0800

    Updated. need test

--- decoder_sig.v
+++ decoder_sig.v
@@ -1,75 +1,57 @@
 module decoder_sig(
-	output wire [6:0] display,
-	output wire [3:0] digit,
-	inout wire PS2_DATA,
-	inout wire PS2_CLK,
 	input wire rst,
-	input wire clk
+	input wire clk,
+	input wire been_ready,
+	input wire [8:0] last_change,
+	input wire [511:0] key_down,
+	output reg[3:0] nums
 	);
 	
 	parameter [8:0] LEFT_SHIFT_CODES  = 9'b0_0001_0010;
 	parameter [8:0] RIGHT_SHIFT_CODES = 9'b0_0101_1001;
 	parameter [8:0] KEY_CODES_UP = 9'b0_0111_0101;	// E075
 	parameter [8:0] KEY_CODES_DOWN = 9'b0_0111_0010;	// E072
-    parameter [8:0] KEY_CODES_LIFT = 9'b0_0110_1011;	// E06B
+    parameter [8:0] KEY_CODES_LEFT = 9'b0_0110_1011;	// E06B
     parameter [8:0] KEY_CODES_RIGHT = 9'b0_0111_0100; // E074
 	parameter [8:0] KEY_CODES_Z = 9'b0_0001_1010; // 1A
 	
-	reg [15:0] nums;
-	reg [3:0] key_num;
-	reg [9:0] last_key;
-	
-	wire shift_down;
-	wire [511:0] key_down;
-	wire [8:0] last_change;
-	wire been_ready;
-	
-	assign shift_down = (key_down[LEFT_SHIFT_CODES] == 1'b1 || key_down[RIGHT_SHIFT_CODES] == 1'b1) ? 1'b1 : 1'b0;
-	
-	SevenSegment seven_seg (
-		.display(display),
-		.digit(digit),
-		.nums(nums),
-		.rst(rst),
-		.clk(clk)
-	);
+	reg [3:0] nums;
+	reg [3:0] nt_nums;
 		
-	KeyboardDecoder key_de (
-		.key_down(key_down),
-		.last_change(last_change),
-		.key_valid(been_ready),
-		.PS2_DATA(PS2_DATA),
-		.PS2_CLK(PS2_CLK),
-		.rst(rst),
-		.clk(clk)
-	);
-
-	always @ (posedge clk, posedge rst) begin
-		if (rst) begin
-			nums <= 16'b0;
-		end else begin
-			nums <= nums;
-			if (been_ready && key_down[last_change] == 1'b1) begin
-				if (key_num != 4'b1111)begin
-					if (shift_down == 1'b1) begin
-						nums <= {key_num, nums[15:4]};
-					end else begin
-						nums <= {nums[11:0], key_num};
-					end
-				end
-			end
+	always @ (posedge clk, posedge rst)
+	begin
+		if (rst)
+		begin
+			nums <= 4'b0000;
+		end
+		else
+		begin
+			nums <= nt_nums;
 		end
 	end
 	
-	always @ (*) begin
-		case (last_change)
-			KEY_CODES_UP : key_num = 4'b0000; //up
-			KEY_CODES_DOWN : key_num = 4'b0001; //down
-			KEY_CODES_LIFT : key_num = 4'b0010; //left
-			KEY_CODES_RIGHT : key_num = 4'b0011; //right
-			KEY_CODES_Z : key_num = 4'b0100; //Z
-			default		  : key_num = 4'b1111;
-		endcase
+	always@(*)begin
+		if (been_ready)begin
+			if(key_down[last_change])begin
+				case(last_change)
+					KEY_CODES_UP:nt_nums[3] = 1'b1;
+					KEY_CODES_DOWN:nt_nums[2] = 1'b1;
+					KEY_CODES_LEFT:nt_nums[1] = 1'b1;
+					KEY_CODES_RIGHT:nt_nums[0] = 1'b1;
+				endcase
+			end
+			else begin
+				case(last_change)
+					KEY_CODES_UP:nt_nums[3] = 1'b0;
+					KEY_CODES_DOWN:nt_nums[2] = 1'b0;
+					KEY_CODES_LEFT:nt_nums[1] = 1'b0;
+					KEY_CODES_RIGHT:nt_nums[0] = 1'b0;
+				endcase
+			end
+		end
+		else begin
+			nt_nums = nums;
+		end
 	end
 	
 endmodule
