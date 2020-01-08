e4f75e30a33f0cbc34d44cf33b416203ff26433f
c22329076 <59045292+c22329076@users.noreply.github.com>
2020-01-09 04:44:37 +0800

    Update score.v

--- score.v
+++ score.v
@@ -10,11 +10,18 @@ module score(
 	input [6:0]enmhp3,
 	input [6:0]enmhp4,
 	input [9:0]bosshp,
-	output reg [3:0]score0,//≠”
-	output reg [3:0]score1,//§Q
-	output reg [3:0]score2,//¶ 
-	output reg [3:0]score3 //§d
+	output reg [3:0]score0,//ÂÄã
+	output reg [3:0]score1,//ÂçÅ
+	output reg [3:0]score2,//Áôæ
+	output reg [3:0]score3 //ÂçÉ
 	);
+	
+	wire count0, count1, count2, count3;
+	
+	assign count0 = (score0 > 4'b1001);
+	assign count1 = (score1 > 4'b1001);
+	assign count2 = (score2 > 4'b1001);
+	assign count3 = (score3 > 4'b1001);
 	
 	reg [3:0] nt_score0;
 	reg [3:0] nt_score1;
@@ -93,57 +100,49 @@ module score(
 
 	always@(*)//score
 	begin
-		if(score0 > 4'b1001)//∂i¶Ï
+		if(shot_enm)//shot enemy, socre + 1
 		begin
-			nt_score0 = score0 - 4'b1010;
-			nt_score1 = score1 + 4'b0001;
+			nt_score0 = score0 + 4'b0001;
 		end
-		else if(score1 > 4'b1001)
+		else if(shot_boss)//shot boss, socre + 2
 		begin
-			nt_score1 = score1 - 4'b1010;
+			nt_score0 = score0 + 4'b0010;
+		end
+		else if((enmhp1 == 7'd0 && !enm[0]) || (enmhp2 == 7'd0 && !enm[1]) || (enmhp3 == 7'd0 && !enm[2]) || (enmhp4 == 7'd0 && !enm[3]))
+		begin
 			nt_score2 = score2 + 4'b0001;
 		end
-		else if(score2 > 4'b1001)
+		else if(shot_reimu)
 		begin
-			nt_score2 = score2 - 4'b1010;
-			nt_score3 = score3 + 4'b0001;
+		    nt_score0 = 4'b0000;
+			nt_score1 = 4'b0000;
+			nt_score2 = 4'b0000;
+			nt_score3 = 4'b0000;
 		end
-		else if(score3 >4'b1001)
+		else if(bosshp == 10'd0 && !boss)//boss die, socre + 1000
 		begin
-			nt_score3 = 4'b1001;
+			nt_score3 = score3 + 4'b0001;
 		end
 		else
 		begin
-		  if(shot_enm)//shot enemy, socre + 1
+						if(count0)//ÈÄ≤‰Ωç
 			begin
-				nt_score0 = score0 + 4'b0001;
+				nt_score0 = score0 - 4'b1010;
+				nt_score1 = score1 + 4'b0001;
 			end
-			else if(shot_boss)//shot boss, socre + 2
+			else if(count1)
 			begin
-				nt_score0 = score0 + 4'b0010;
-			end
-			else if(enmhp1 == 7'd0 && !enm[0])//enemy die, socre + 100
-			begin
+				nt_score1 = score1 - 4'b1010;
 				nt_score2 = score2 + 4'b0001;
 			end
-			else if(enmhp2 == 7'd0 && !enm[1])//enemy die, socre + 100
+			else if(count2)
 			begin
-				nt_score2 = score2 + 4'b0001;
+				nt_score2 = score2 - 4'b1010;
+				nt_score3 = score3 + 4'b0001;
 			end
-			else if(enmhp3 == 7'd0 && !enm[2])//enemy die, socre + 100
+			else if(count3)
 			begin
-				nt_score2 = score2 + 4'b0001;
-			end
-			else if(enmhp4 == 7'd0 && !enm[3])//enemy die, socre + 100
-			begin
-				nt_score2 = score2 + 4'b0001;
-			end
-			else if(shot_reimu)
-			begin
-			    nt_score0 = 4'b0000;
-				nt_score1 = 4'b0000;
-				nt_score2 = 4'b0000;
-				nt_score3 = 4'b0000;
+				nt_score3 = 4'b1001;
 			end
 			else
 			begin
@@ -151,14 +150,6 @@ module score(
 				nt_score1 = score1;
 				nt_score2 = score2;
 				nt_score3 = score3;
-			end
-			if(bosshp == 10'd0 && !boss)//boss die, socre + 1000
-			begin
-				nt_score3 = score3 + 4'b0001;
-			end
-			else
-			begin
-			    nt_score3 = score3;
 			end
 		end
 	end
